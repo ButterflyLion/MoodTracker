a8d3689f9888a4c974ee3f5ff500d34a1f3bb315
ButterflyLion <orosztama@gmail.com>
2025-03-23 20:15:07 +0000

    tutorial 4 completed - added image picker

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,19 +1,52 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image, ActivityIndicator } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import React, { useState } from "react";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require("@/assets/images/background-image.jpg");
+const PlaceholderImageUri = Image.resolveAssetSource(PlaceholderImage).uri;
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      console.log(result);
+    } else {
+      alert("You didn't select any image.");
+    }
+  };
+
+  const resetImage = () => {
+    setSelectedImage(PlaceholderImageUri);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
-        <Button label="Use default photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use default photo" onPress={resetImage} />
       </View>
     </View>
   );
--- components/ImageViewer.tsx
+++ components/ImageViewer.tsx
@@ -1,18 +1,43 @@
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, ActivityIndicator, StyleSheet } from "react-native";
 import { Image, type ImageSource } from "expo-image";
 
 type Props = {
   imgSource: ImageSource;
+  selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource }: Props) {
-  return <Image source={imgSource} style={styles.image} />;
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : imgSource;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+  }, [selectedImage]);
+
+  return (
+    <View style={styles.container}>
+      {loading && <ActivityIndicator size="large" color="#fff" />}
+      <Image
+        source={imageSource}
+        style={styles.image}
+        onLoadEnd={() => setLoading(false)}
+        onError={() => setLoading(false)}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   image: {
     width: 300,
-    height: 400,
-    borderRadius: 18,
+    height: 300,
+    borderRadius: 20,
+    position: "absolute",
   },
 });
